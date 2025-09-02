148b647354934ddd8c7106fdb08e134406301a61
Federico Galatolo <galatolo.federico@gmail.com>
2025-09-02 16:31:57 +0200

    removed old build file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# mkl
-
-RUSTFLAGS="-C target-cpu=native" cargo run --release --bin benchmark --features "candle-core/mkl candle-nn/mkl candle-transformers/mkl" -- --quantized --quantized-weights ./q8.gguf
-
-# cuda
-
-cargo run --release --bin benchmark --features "candle-core/cuda candle-nn/cuda candle-transformers/cuda" -- --quantized --quantized-weights ./q8.gguf
